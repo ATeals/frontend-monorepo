df52340ceabf0557438b5a3cd81490b53c11728c
ateals <ateals@icloud.com>
2024-02-22 23:21:46 +0900

    feat: mornorepo 환경 세팅
    mornorepo 환경 세팅

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom/extend-expect";
+
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^.+\\.svg$": "jest-svg-transformer",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
