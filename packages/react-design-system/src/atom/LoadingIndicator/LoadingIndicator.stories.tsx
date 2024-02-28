f0f1db13af7e253cca3af2cd78cac9216db0d25b
ateals <ateals@icloud.com>
2024-02-29 00:25:25 +0900

    feat(#1): Atom / LoadingIndicator

---
+++ packages/react-design-system/src/atom/LoadingIndicator/LoadingIndicator.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { LoadingIndicator } from ".";
+
+const meta = {
+  title: "Atoms/LoadingIndicator",
+  component: LoadingIndicator,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof LoadingIndicator>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Small: Story = {
+  args: {
+    fill: "black",
+  },
+};
