c9d9bc021a35bf57a9e1c64f7786c129ce38921a
ateals <ateals@icloud.com>
2024-03-13 14:24:33 +0900

    fix: ishangul -> isHangul

--- packages/typescript-utils/src/hangul/disassembleHangul.ts
+++ packages/typescript-utils/src/hangul/disassembleHangul.ts
@@ -1,7 +1,7 @@
-import { getChosung, ishangul, splitHangul } from "./utils";
+import { getChosung, isHangul, splitHangul } from "./utils";
 
 export const disassembleChosung = (hangul: string) =>
-  hangul.split("").map((char) => (ishangul(char) ? getChosung(char) : char));
+  hangul.split("").map((char) => (isHangul(char) ? getChosung(char) : char));
 
 export const disassembleHangul = (
   hangul: string,
@@ -10,7 +10,7 @@ export const disassembleHangul = (
   const wordList = hangul.split("");
 
   return wordList.reduce((list: Array<string | string[]>, word) => {
-    if (ishangul(word)) {
+    if (isHangul(word)) {
       const words = splitHangul(word);
       return isGroupedByCharacter ? [...list, words] : [...list, ...words];
     } else {
